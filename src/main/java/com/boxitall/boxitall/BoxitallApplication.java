455d6517c56d40e5c7bc8778dfaabed113e51bb3
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-05-26 00:26:58 -0300

    First commit

---
+++ src/main/java/com/boxitall/boxitall/BoxitallApplication.java
@@ -0,0 +1,13 @@
+package com.boxitall.boxitall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxitallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxitallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boxitall/boxitall/BoxitallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxitall.boxitall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxitallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
