714921f0cf83451c81de37d2b3a08836bd4ed86d
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-05-26 00:37:15 -0300

    connect to db

--- src/main/java/com/boxitall/boxitall/BoxitallApplication.java
+++ src/main/java/com/boxitall/boxitall/BoxitallApplication.java
@@ -1,5 +1,6 @@
 package com.boxitall.boxitall;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoxitallApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMalformed()
+				.ignoreIfMissing()
+				.load();
+
+		// Load env vars into JVM
+		dotenv.entries().forEach((entry) -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(BoxitallApplication.class, args);
 	}
 
