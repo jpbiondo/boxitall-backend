1e10a201c0537b54f5f025d6fa56db6ca0329102
lucilaoberti <mlucilaoberti@gmail.com>
2025-06-26 13:33:41 -0300

    Eliminar detalle ajuste

--- src/main/java/com/boxitall/boxitall/services/OrdenCompraService.java
+++ src/main/java/com/boxitall/boxitall/services/OrdenCompraService.java
@@ -116,20 +116,30 @@ public class OrdenCompraService extends BaseEntityServiceImpl<OrdenCompra, Long>
         try {
             OrdenCompra orden = ordenCompraRepository.findById(idOrden)
                     .orElseThrow(() -> new RuntimeException("Orden de compra no encontrada."));
+
             OrdenCompraEstadoOC estadoActual = orden.getEstadoActual(orden);
             OrdenCompraArticulo detalle = ordenCompraArticuloRepository.findById(idDetalle)
                     .orElseThrow(() -> new RuntimeException("Detalle no encontrado."));
-            // verfica si se puede eliminar
+
             String estadoActualNombre = estadoActual.getEstado().getNombre();
             if (!"PENDIENTE".equals(estadoActualNombre)) {
                 throw new RuntimeException("Solo se pueden modificar órdenes en estado PENDIENTE.");
             }
+
+            // Eliminar el detalle
             orden.getDetalles().remove(detalle);
-            ordenCompraRepository.save(orden);
+
+            // Si no quedan más detalles, eliminar la orden completa
+            if (orden.getDetalles().size() == 0) {
+                ordenCompraRepository.delete(orden);
+            } else {
+                ordenCompraRepository.save(orden);
+            }
         } catch (Exception e) {
-            throw new RuntimeException("Error eliminar el detalle de la orden de compra: " + e.getMessage(), e);
+            throw new RuntimeException("Error al eliminar el detalle de la orden de compra: " + e.getMessage(), e);
         }
     }
+
     @Transactional
     public void actualizarCantidadDetalle(Long idOrden, Long idDetalle, Integer nuevaCantidad) {
         try{
