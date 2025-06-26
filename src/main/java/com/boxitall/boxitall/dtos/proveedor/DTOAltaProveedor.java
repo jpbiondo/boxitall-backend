9a83edc312c03d6a91efffd00af9e38d57b803f2
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-06-25 22:47:04 -0300

    salame

--- src/main/java/com/boxitall/boxitall/dtos/proveedor/DTOAltaProveedor.java
+++ src/main/java/com/boxitall/boxitall/dtos/proveedor/DTOAltaProveedor.java
@@ -3,10 +3,16 @@ package com.boxitall.boxitall.dtos.proveedor;
 import com.boxitall.boxitall.dtos.articulo.DTOArticuloAddProveedor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
+@ToString
 public class DTOAltaProveedor {
-    private DTOProveedor dtoProveedor;
-    private DTOArticuloAddProveedor dtoArticuloAddProveedor;
+    private int codigo;
+    private String nombre;
+    private String telefono;
+    private List<DTOArticuloAddProveedor> proveedorArticulos;
 }
